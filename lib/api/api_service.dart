fb6228c11a51b43a49295ad87051fc4db0e568fd
pablo014 <angelopablo014@gmail.com>
2022-11-14 23:31:18 -0600

    feat(api): got display on page

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
@@ -8,21 +9,25 @@ class PokeLink {
   final String url;
 
   const PokeLink({required this.name, required this.url});
+
+  factory PokeLink.fromJson(Map<String, dynamic> json) {
+    return PokeLink(name: json['name'], url: json['url']);
+  }
 }
 
 class ApiService {
   Future<List<PokeLink>> getPokeList() async {
-      var url = Uri.https(ApiConstants.baseUrl);
-      var response = await http.get(url);
-      if (response.statusCode == 200) {
-        Iterable l = jsonDecode(response.body);
-        List<PokeLink> i = [];
-        for (var e in l) {
-          i.add(PokeLink(name: e['name'], url: e['url']));
-        }
-        return i;
-      } else {
-        return [];
+    var url = Uri.parse(ApiConstants.baseUrl);
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      LinkedHashMap l = jsonDecode(response.body);
+      List<PokeLink> i = [];
+      for (Map<String, dynamic> e in l['results']) {
+        i.add(PokeLink.fromJson(e));
       }
-}
+      return i;
+    } else {
+      return [];
+    }
+  }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../api/api_service.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -8,11 +10,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late Future<List<PokeLink>> pokeList;
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  void getData() async {
+    pokeList = ApiService().getPokeList();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('test'),
-    );
+    return FutureBuilder<List<PokeLink>>(
+      future: pokeList,
+        builder: (context, snapshot) {
+        if(snapshot.hasData) {
+          return ListView(
+            children: [
+              for (var item in snapshot.data!)
+                Text(item.name)
+            ],
+          );
+        }
+        else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        } else {
+          return const CircularProgressIndicator();
+        }
+        });
   }
 }
